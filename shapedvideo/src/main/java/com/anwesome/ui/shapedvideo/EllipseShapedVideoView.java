909241ed8215757c99ac36ae7ca82f9fef4331b5
Anwesh43 <anweshthecool0@gmail.com>
2017-02-07 15:07:46 +0530

    Adding EllipseShapedVideoView

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/EllipseShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/EllipseShapedVideoView.java
@@ -1,7 +1,24 @@
 package com.anwesome.ui.shapedvideo;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.VideoView;
+
 /**
  * Created by anweshmishra on 07/02/17.
  */
-public class EllipseShapedVideoView {
+public class EllipseShapedVideoView extends ShapedVideoView {
+    public EllipseShapedVideoView(Context context) {
+        super(context);
+    }
+    public EllipseShapedVideoView(Context context, AttributeSet attributeSet) {
+        super(context,attributeSet);
+    }
+    public Path definePath(Path path,int w,int h) {
+        path.addArc(new RectF(0,0,w,h),0,360);
+        return path;
+    }
 }
