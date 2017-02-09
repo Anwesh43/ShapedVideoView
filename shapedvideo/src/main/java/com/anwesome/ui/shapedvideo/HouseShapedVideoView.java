42758016eb626840490c71e8f35d2cefb4fd39c7
Anwesh43 <anweshthecool0@gmail.com>
2017-02-10 02:47:36 +0530

    chaning width

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/HouseShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/HouseShapedVideoView.java
@@ -17,7 +17,7 @@ public class HouseShapedVideoView extends ShapedVideoView {
     public Path definePath(Path path,int w,int h) {
         path.moveTo(0,h/2);
         path.lineTo(w/2,0);
-        path.lineTo(w/2,0);
+        path.lineTo(w,h/2);
         path.lineTo(w,h);
         path.lineTo(0,h);
         path.lineTo(0,h/2);
