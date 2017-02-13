5fe9d3b14a54c50a0eac8c8839ef59fec01fa130
Anwesh43 <anweshthecool0@gmail.com>
2017-02-13 14:41:24 +0530

    Adding diamondshapedvideoview

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DiamondShapedVideoView.java
@@ -0,0 +1,22 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.Path;
+import android.util.AttributeSet;
+
+/**
+ * Created by anweshmishra on 13/02/17.
+ */
+public class DiamondShapedVideoView extends ShapedVideoView{
+    public DiamondShapedVideoView(Context context,AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public Path definPath(Path path,int w,int h) {
+        path.moveTo(0,h/2);
+        path.lineTo(w/2,0);
+        path.lineTo(w,h/2);
+        path.lineTo(w/2,h/2);
+        path.lineTo(0,h/2);
+        return path;
+    }
+}
