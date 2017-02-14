0eed8fb8352f167d920bf78177c8fa95ef76d00d
Anwesh43 <anweshthecool0@gmail.com>
2017-02-14 15:44:17 +0530

    Adding Constants file

--- app/src/main/java/com/anwesome/ui/shapedvideoview/AppConstants.java
+++ app/src/main/java/com/anwesome/ui/shapedvideoview/AppConstants.java
@@ -4,4 +4,6 @@ package com.anwesome.ui.shapedvideoview;
  * Created by anweshmishra on 14/02/17.
  */
 public class AppConstants {
+    public static final String MAIN_URL  = "http://192.168.0.3:8000/android-images";
+
 }
