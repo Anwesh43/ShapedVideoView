d2c7fa697f04e65d06ca947fda9b92e5248e0c2a
Anwesh43 <anweshthecool0@gmail.com>
2017-02-17 18:40:53 +0530

    Positioning changes

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/EightShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/EightShapedVideoView.java
@@ -14,11 +14,12 @@ public class EightShapedVideoView extends ShapedVideoView{
     }
     public Path definePath(Path path,int w,int h) {
         float r = h/4;
-        float y_center1 = h/2+h/4,y_center2 = h/2-h/4;
+        float y_center1 = h/2+h/4;
         float x = (float)(w/2+r*Math.cos(-60*Math.PI/180)),y = (float)(y_center1+r*Math.sin(-60*Math.PI/180));
+        float y_center2 = y-r*(float)Math.cos(Math.PI/3);
         path.moveTo(x,y);
         path.arcTo(new RectF(0,h/2,w,h),-60,300);
-        path.arcTo(new RectF(0,0,w,h/2),120,300);
+        path.arcTo(new RectF(0,y_center2-r,w,y_center2+r),120,300);
         return path;
     }
 }
