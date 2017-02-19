731295a27559d6579c8aee17b20990b1d985a689
Anwesh43 <anweshthecool0@gmail.com>
2017-02-19 16:39:19 +0530

    Resizing video view

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/SemiCircRectShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/SemiCircRectShapedVideoView.java
@@ -2,6 +2,7 @@ package com.anwesome.ui.shapedvideo;
 
 import android.content.Context;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 
 /**
@@ -12,13 +13,13 @@ public class SemiCircRectShapedVideoView extends ShapedVideoView {
         super(context,attrs);
     }
     public Path definePath(Path path,int w,int h) {
-        path.moveTo(w/2+w/4,h/2) ;
-        path.lineTo(w,h/2);
+        path.moveTo(w/2+w/4,h/3) ;
+        path.lineTo(w,h/3);
         path.lineTo(w,h);
         path.lineTo(0,h);
-        path.lineTo(0,h/2);
-        path.lineTo(w/2-w/4,h/2);
-        path.addCircle(w/2,h/2,w/4, Path.Direction.CCW);
+        path.lineTo(0,h/3);
+        path.lineTo(w/2-w/4,h/3);
+        path.addArc(new RectF(w/2-w/4,h/3-w/4,w/2+w/4,h/3+w/4),180,180);
         return path;
     }
 }
