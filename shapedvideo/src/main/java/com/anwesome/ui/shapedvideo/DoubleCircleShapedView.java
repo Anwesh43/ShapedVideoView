2a1fa2dacd4fef44e70b4bf50be5055863644ccc
Anwesh43 <anweshthecool0@gmail.com>
2017-02-23 15:22:16 +0530

    making the circle as ellipse

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DoubleCircleShapedView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DoubleCircleShapedView.java
@@ -1,11 +1,8 @@
 package com.anwesome.ui.shapedvideo;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.*;
-import android.net.Uri;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 
 /**
  * Created by anweshmishra on 23/02/17.
@@ -19,8 +16,8 @@ public class DoubleCircleShapedView extends ShapedVideoView {
     }
     public Path definePath(Path path,int w,int h) {
         for(int i=0;i<4;i++) {
-            int x = w/2*(i%2)+w/4,y = h/2*(i/2)+h/4,r = w/4;
-            path.addCircle(x,y,r, Path.Direction.CCW);
+            int x = w/2*(i%2)+w/4,y = h/2*(i/2)+h/4,r1=w/4,r2 =h/4;
+            path.addArc(new RectF(x-r1,y-r2,x+r1,y+r2),0,360);
         }
         return path;
     }
