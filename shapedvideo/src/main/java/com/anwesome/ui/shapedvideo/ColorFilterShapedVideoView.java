d672c4ab734e7492033d3e182a2c19c8e46c2114
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 12:05:57 +0530

    Handling fling gesture to move current and prev filter rects

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ColorFilterShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ColorFilterShapedVideoView.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.shapedvideo;
 import android.content.Context;
 import android.graphics.*;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -13,6 +14,8 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class ColorFilterShapedVideoView extends ShapedVideoView{
     private int time = 0;
     private boolean isAnimated = false;
+    private int currIndex = 0;
+    private ColorFilterRect currRect  = null,prevRect = null;
     private ConcurrentLinkedQueue<ColorFilterRect> rects = new ConcurrentLinkedQueue<>();
     private String colorHexes[] = {"#AAF57C00","#AAE53935","#AA43A047","#AA0277BD"};
     public ColorFilterShapedVideoView(Context context) {
@@ -42,6 +45,18 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
             }
         }
     }
+    private ColorFilterRect getIndex(int i) {
+        int index = 0;
+        ColorFilterRect colorFilterRect = null;
+        for(ColorFilterRect rect:rects) {
+            if(index == i) {
+                colorFilterRect = rect;
+                break;
+            }
+            index++;
+        }
+        return colorFilterRect;
+    }
     private void initRects(int w,int h) {
         for(int i=0;i<colorHexes.length;i++) {
             rects.add(new ColorFilterRect(i*w,0,w,h,Color.parseColor(colorHexes[i])));
@@ -52,6 +67,7 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
     }
     private class ColorFilterRect {
         private float x,y,finalX;
+
         private int w,h,color,speed,dir = 0;
         public ColorFilterRect(float x,float y,int w,int h,int color) {
             this.x = x;
@@ -61,6 +77,12 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
             this.finalX = x;
             this.speed = w/10;
             this.color = color;
+        }
+        public float getNextX(int dir) {
+            return x+w*dir;
+        }
+        public float getPrevX(int dir) {
+            return x-w*dir;
         }
         public void draw(Canvas canvas,Paint paint) {
             paint.setColor(color);
@@ -82,6 +104,48 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
         }
         public int hashCode() {
             return color+(int)x+(int)y;
+        }
+        public void setX(float x) {
+            this.x = x;
+        }
+    }
+    private class FilterGestureListener extends GestureDetector.SimpleOnGestureListener {
+        public boolean onDown(MotionEvent event) {
+            return true;
+        }
+        public boolean onSingleTapUp(MotionEvent event) {
+            return true;
+        }
+        public boolean onFling(MotionEvent e1,MotionEvent e2,float velx,float vely) {
+            if(prevRect == null && currRect == null && !isAnimated) {
+                if(e1.getX()<e2.getX()) {
+                    prevRect = getIndex(currIndex);
+                    if(currIndex == 0){
+                        currRect = getIndex(rects.size()-1);
+                    }
+                    if(prevRect!=null  && currRect!=null){
+                        prevRect.startMoving(1,prevRect.getNextX(1));
+                        currRect.setX(prevRect.getPrevX(1));
+                        currRect.startMoving(1,0);
+                    }
+                }
+                else if(e1.getX()>e2.getX()) {
+                    prevRect = getIndex(currIndex);
+                    if(currIndex == rects.size()-1){
+                        currRect = getIndex(0);
+                    }
+                    if(prevRect!=null  && currRect!=null){
+                        prevRect.startMoving(-1,prevRect.getNextX(-1));
+                        currRect.setX(prevRect.getPrevX(-1));
+                        currRect.startMoving(-1,0);
+                    }
+                }
+                if(prevRect!=null && currRect!=null) {
+                    isAnimated = true;
+                    postInvalidate();
+                }
+            }
+            return true;
         }
     }
 }
