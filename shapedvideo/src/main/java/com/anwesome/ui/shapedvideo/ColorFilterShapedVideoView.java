916e40a604ecd56c2712cd83e81df133c6f81497
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 11:49:17 +0530

    Drawing colorRects

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ColorFilterShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ColorFilterShapedVideoView.java
@@ -5,11 +5,16 @@ import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * Created by anweshmishra on 27/02/17.
  */
 public class ColorFilterShapedVideoView extends ShapedVideoView{
     private int time = 0;
+    private boolean isAnimated = false;
+    private ConcurrentLinkedQueue<ColorFilterRect> rects = new ConcurrentLinkedQueue<>();
+    private String colorHexes[] = {"#AAF57C00","#AAE53935","#AA43A047","#AA0277BD"};
     public ColorFilterShapedVideoView(Context context) {
         super(context);
     }
@@ -20,7 +25,27 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
         return  true;
     }
     public void drawElements(Canvas canvas, Paint paint) {
+        if(time == 0) {
+            initRects(canvas.getWidth(),canvas.getHeight());
+        }
+        for(ColorFilterRect rect:rects) {
+            rect.draw(canvas,paint);
+        }
+        time++;
+        if(isAnimated) {
+            try {
+                Thread.sleep(50);
+                invalidate();
+            }
+            catch (Exception ex) {
 
+            }
+        }
+    }
+    private void initRects(int w,int h) {
+        for(int i=0;i<colorHexes.length;i++) {
+            rects.add(new ColorFilterRect(i*w,0,w,h,Color.parseColor(colorHexes[i])));
+        }
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
@@ -38,7 +63,8 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
             this.color = color;
         }
         public void draw(Canvas canvas,Paint paint) {
-
+            paint.setColor(color);
+            canvas.drawRect(new RectF(x,y,x+w,y+h),paint);
         }
         public void startMoving(int dir,float finalX) {
             this.dir = dir;
