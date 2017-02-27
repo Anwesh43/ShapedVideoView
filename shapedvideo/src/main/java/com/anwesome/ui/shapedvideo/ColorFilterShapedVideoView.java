407c760c9e3233e24211efb65e7ec515afd461ef
Anwesh43 <anweshthecool0@gmail.com>
2017-02-27 11:39:11 +0530

    Adding ColorFilter rect

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ColorFilterShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ColorFilterShapedVideoView.java
@@ -1,8 +1,7 @@
 package com.anwesome.ui.shapedvideo;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
@@ -10,6 +9,7 @@ import android.view.MotionEvent;
  * Created by anweshmishra on 27/02/17.
  */
 public class ColorFilterShapedVideoView extends ShapedVideoView{
+    private int time = 0;
     public ColorFilterShapedVideoView(Context context) {
         super(context);
     }
@@ -24,5 +24,38 @@ public class ColorFilterShapedVideoView extends ShapedVideoView{
     }
     public boolean onTouchEvent(MotionEvent event) {
         return true;
+    }
+    private class ColorFilterRect {
+        private float x,y,finalX;
+        private int w,h,color,speed,dir = 0;
+        public ColorFilterRect(float x,float y,int w,int h,int color) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+            this.finalX = x;
+            this.speed = w/10;
+            this.color = color;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+
+        }
+        public void startMoving(int dir,float finalX) {
+            this.dir = dir;
+            this.finalX = finalX;
+        }
+        public boolean stop() {
+            return dir == 0;
+        }
+        public void move() {
+            this.x+=dir;
+            if((dir == 1 && x>=finalX) || (dir == -1 && x<=finalX)) {
+                x = finalX;
+                dir = 0;
+            }
+        }
+        public int hashCode() {
+            return color+(int)x+(int)y;
+        }
     }
 }
