27cfdf9fae53aedbfa250b3326a2e871ee1b330f
Anwesh43 <anweshthecool0@gmail.com>
2017-03-01 12:01:11 +0530

    Adding the methods for the circularcolorfilter

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CircularColorFilterShapeVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CircularColorFilterShapeVideoView.java
@@ -1,7 +1,35 @@
 package com.anwesome.ui.shapedvideo;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.graphics.*;
+import android.view.MotionEvent;
+
 /**
  * Created by anweshmishra on 01/03/17.
  */
-public class CircularColorFilterShapeVideoView {
+public class CircularColorFilterShapeVideoView extends ShapedVideoView{
+    private boolean isAnimated = false;
+    public CircularColorFilterShapeVideoView(Context context) {
+        super(context);
+    }
+    public CircularColorFilterShapeVideoView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public boolean shouldDraw() {
+        return true;
+    }
+    public void onDraw(Canvas canvas) {
+        if(isAnimated) {
+            try {
+                Thread.sleep(50);
+                invalidate();
+            } catch (Exception ex) {
+
+            }
+        }
+    }
+    public void handleTap(float x,float y) {
+
+    }
 }
