20f28fae144c55fc6dee16b5472d2c92706190ec
Anwesh43 <anweshthecool0@gmail.com>
2017-03-02 10:53:58 +0530

    Adding PacCreator ShapedVideoView

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/PacCreateShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/PacCreateShapedVideoView.java
@@ -1,7 +1,35 @@
 package com.anwesome.ui.shapedvideo;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
 /**
  * Created by anweshmishra on 02/03/17.
  */
-public class PacCreateShapedVideoView {
+public class PacCreateShapedVideoView extends ShapedVideoView {
+    public PacCreateShapedVideoView(Context context){
+        super(context);
+    }
+    public PacCreateShapedVideoView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+    public boolean shouldDraw() {
+        return true;
+    }
+    public void onDraw(Canvas canvas) {
+        try {
+            Thread.sleep(100);
+            invalidate();
+        }
+        catch (Exception ex) {
+
+        }
+    }
+    private class PacCreator {
+        public PacCreator() {
+
+        }
+    }
+    
 }
