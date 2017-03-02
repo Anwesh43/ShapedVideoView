60d7f94f84579e12ea3e31526de59ea0ec2c94d3
Anwesh43 <anweshthecool0@gmail.com>
2017-03-03 03:01:02 +0530

    Adding SquareColorFilter class

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/SquaredColorFilterVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/SquaredColorFilterVideoView.java
@@ -3,6 +3,8 @@ package com.anwesome.ui.shapedvideo;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.media.UnsupportedSchemeException;
 import android.util.AttributeSet;
 
 /**
@@ -19,6 +21,38 @@ public class SquaredColorFilterVideoView extends ShapedVideoView {
 
     }
     class SquareColorFilter {
-        
+        private int color;
+        private float x,y,w,h,scale = 0,dir = 1;
+        public SquareColorFilter(int color,float...dimen) {
+            if(dimen.length == 4) {
+                this.x = dimen[0];
+                this.y = dimen[1];
+                this.w = dimen[2];
+                this.h = dimen[3];
+            }
+            else {
+                throw  new UnsupportedOperationException();
+            }
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            paint.setColor(color);
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.scale(scale,scale);
+            canvas.drawRect(new RectF(-w/2,-h/2,w/2,h/2),paint);
+            canvas.restore();
+        }
+        public void update() {
+            scale+=dir*0.1f;
+            if(scale>=1) {
+                dir = 0;
+            }
+        }
+        public boolean stopped() {
+            return dir == 0;
+        }
+        public int hashCode() {
+            return (int)x+(int)y+color;
+        }
     }
 }
