36983742104709c19837afa5f6e331a29301198d
Anwesh43 <anweshthecool0@gmail.com>
2017-03-04 12:45:13 +0530

    Adding Loader

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/LazyLoadShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/LazyLoadShapedVideoView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.shapedvideo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 
 /**
@@ -15,8 +17,28 @@ public class LazyLoadShapedVideoView extends ShapedVideoView {
     }
     public void drawElements(Canvas canvas,Paint paint) {
         time++;
+
     }
     public void start() {
 
+    }
+    private class Loader {
+        private float x,y,deg = 0,radius;
+        public Loader(float x,float y,float radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            paint.setColor(Color.GRAY);
+            paint.setStrokeWidth(15);
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.drawArc(new RectF(-radius,-radius,radius,radius),deg,deg+30,false,paint);
+            canvas.restore();
+        }
+        public void update() {
+            deg+=30;
+        }
     }
 }
