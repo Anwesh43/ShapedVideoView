2e55e6b4f2619dd66c905292cd68b80fb9467aa1
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 15:50:09 +0530

    Adding triangle color filter

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/TriangleColorFilterShapedVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/TriangleColorFilterShapedVideoView.java
@@ -15,5 +15,34 @@ public class TriangleColorFilterShapedVideoView extends ShapedVideoView {
     public void handleTap(float x,float y) {
 
     }
-    
+    private class TriangleColorFilter {
+        private float x,y,size,scale=0,rot=0,dir =0;
+        public TriangleColorFilter(float x,float y,float size) {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.scale(scale,scale);
+            canvas.rotate(rot);
+            canvas.restore();
+        }
+        public void update() {
+            rot+=10*dir;
+        }
+        public void show() {
+            dir  = 1;
+        }
+        public void clear() {
+            dir = -1;
+        }
+        public boolean stopped() {
+            return dir == 0;
+        }
+        public int hashCode() {
+            return (int)(scale+rot);
+        }
+    }
 }
