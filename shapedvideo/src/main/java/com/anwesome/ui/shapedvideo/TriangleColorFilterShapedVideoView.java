c939c3ce07a4441e9f49389f508bfc1f48136816
Anwesh43 <anweshthecool0@gmail.com>
2017-03-06 15:30:03 +0530

    adding triangle color filter

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/TriangleColorFilterShapedVideoView.java
@@ -0,0 +1,19 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.*;
+/**
+ * Created by anweshmishra on 06/03/17.
+ */
+public class TriangleColorFilterShapedVideoView extends ShapedVideoView {
+    public TriangleColorFilterShapedVideoView(Context context) {
+        super(context);
+    }
+    public void drawElements(Canvas canvas,Paint paint) {
+
+    }
+    public void handleTap(float x,float y) {
+
+    }
+    
+}
