0f72b8ba542f868fd0c6331f8aff5a283e8abeb3
Anwesh43 <anweshthecool0@gmail.com>
2017-03-13 18:16:50 +0530

    minor fixes in x and y position

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawLineVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawLineVideoView.java
@@ -72,7 +72,7 @@ public class DrawLineVideoView extends ShapedVideoView {
             return Math.sqrt(Math.pow(x-startX,2)+Math.pow(y-startY,2));
         }
         public void computeInnerPoints(float endX,float endY) {
-            float theta = (endX == startX)?90:Math.abs(endY-startY)/(Math.abs(endX-startX));
+            float theta = (endX == startX)?90:(float)Math.atan((Math.abs(endY-startY))/(Math.abs(endX-startX)));
             float xDir = endX-startX == 0?0:(endX-startX)/Math.abs(endX-startX);
             float yDir = endY-startY == 0?0:(endY-startY)/Math.abs(endY-startY);
             if(xDir == yDir) {
@@ -83,8 +83,8 @@ public class DrawLineVideoView extends ShapedVideoView {
             }
             float x = startX,y = startY;
             while(true) {
-                x+=linePointRadius*Math.cos(theta*Math.PI/180);
-                y+=linePointRadius*Math.sin(theta*Math.PI/180);
+                x+=2*linePointRadius*Math.cos(theta*Math.PI/180);
+                y+=2*linePointRadius*Math.sin(theta*Math.PI/180);
                 if(getDistance(x,y)>getDistance(endX,endY)) {
                     break;
                 }
@@ -148,7 +148,7 @@ public class DrawLineVideoView extends ShapedVideoView {
             if(pointF.y<=initY-100) {
                 pointF.y = initY-100;
                 shouldNeighborJump = true;
-                yDir = 1;
+                yDir = -1;
             }
             else if(pointF.y>=initY){
                 pointF.y = initY;
