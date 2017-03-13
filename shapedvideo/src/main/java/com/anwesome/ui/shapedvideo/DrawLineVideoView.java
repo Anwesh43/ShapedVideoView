cb038c148c84cc1ad67e083c9d3fb0e0c8dcf840
Anwesh43 <anweshthecool0@gmail.com>
2017-03-13 13:57:36 +0530

    Adding DrawLineVideoView

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawLineVideoView.java
@@ -0,0 +1,27 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+/**
+ * Created by anweshmishra on 13/03/17.
+ */
+public class DrawLineVideoView extends ShapedVideoView {
+    public DrawLineVideoView(Context context) {
+        super(context);
+    }
+    public DrawLineVideoView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public boolean shouldDraw() {
+        return true;
+    }
+    public void drawElements(Canvas canvas, Paint paint) {
+
+    }
+    public void handleTap(float x,float y) {
+
+    }
+}
