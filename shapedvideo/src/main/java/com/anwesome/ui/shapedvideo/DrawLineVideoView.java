5fb8d4d7a067dca33023052ceb27cfe54ee7c41d
Anwesh43 <anweshthecool0@gmail.com>
2017-03-13 14:05:41 +0530

    Adding Linepoint

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawLineVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawLineVideoView.java
@@ -3,6 +3,7 @@ package com.anwesome.ui.shapedvideo;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 
 /**
@@ -23,5 +24,31 @@ public class DrawLineVideoView extends ShapedVideoView {
     }
     public void handleTap(float x,float y) {
 
+    }
+    private class LinePoint {
+        private PointF pointF;
+        private float yDir = 0,initY;
+        public LinePoint(PointF pointF) {
+            this.pointF = pointF;
+            this.initY = pointF.y;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            canvas.drawCircle(pointF.x,pointF.y,10,paint);
+        }
+        public int hashCode() {
+            return pointF.hashCode()+(int)yDir;
+        }
+        public void jump() {
+            yDir  = yDir == 0?1:yDir;
+        }
+        public void update() {
+            pointF.y+=yDir*20;
+            if(pointF.y<=initY-100) {
+                pointF.y = initY-100;
+            }
+            else if(pointF.y>=initY){
+                pointF.y = initY;
+            }
+        }
     }
 }
