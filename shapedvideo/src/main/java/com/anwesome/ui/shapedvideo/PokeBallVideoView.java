381edf92a644766bcd23349c8e07926db2f93cf1
Anwesh43 <anweshthecool0@gmail.com>
2017-03-17 16:32:01 +0530

    Adding PokeBallVideoView

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/PokeBallVideoView.java
@@ -0,0 +1,23 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+/**
+ * Created by anweshmishra on 17/03/17.
+ */
+public class PokeBallVideoView extends ShapedVideoView {
+    public PokeBallVideoView(Context context) {
+        super(context);
+    }
+    public PokeBallVideoView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public void drawElements(Canvas canvas, Paint paint) {
+    }
+    public void handleTap(float x,float y) {
+
+    }
+}
