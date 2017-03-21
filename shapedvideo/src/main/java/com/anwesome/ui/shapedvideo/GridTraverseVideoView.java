1c1db0eda208cc38b759b2ab6f5dc3ccfca72f4a
Anwesh43 <anweshthecool0@gmail.com>
2017-03-21 10:12:52 +0530

    Adding some minor fixes

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/GridTraverseVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/GridTraverseVideoView.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by anweshmishra on 21/03/17.
@@ -21,16 +22,24 @@ public class GridTraverseVideoView extends ShapedVideoView {
     public GridTraverseVideoView(Context context, AttributeSet attrs) {
         super(context,attrs);
     }
+    public boolean shouldDraw() {
+        return true;
+    }
     public void drawElements(Canvas canvas, Paint paint) {
         if(time == 0) {
             w = canvas.getWidth();
             h = canvas.getHeight();
+            int na = 0;
             int k = 20;
-            float sizeOfGrid = w/k,x = w/(2*k),y=0;
+            float sizeOfGrid = w/k,x = w/(2*k),y=w/(2*k);
             while(y<h-sizeOfGrid/2) {
                 boolean allowed = true;
-                if(grids.size()%20 == 13) {
-                    allowed = false;
+                if(grids.size()%30 == 11 && na<7) {
+                    Random random = new Random();
+                    allowed = random.nextInt(1000)%2 != 0;
+                    if(!allowed) {
+                        na++;
+                    }
                 }
                 grids.add(new SquareGrid(x,y,sizeOfGrid,allowed));
                 x+=sizeOfGrid;
@@ -47,7 +56,7 @@ public class GridTraverseVideoView extends ShapedVideoView {
                 if(index%k>=1) {
                     grid.setLeft(grids.get(index-1));
                 }
-                if(index%k<=k-1) {
+                if(index%k<=k-1 && index+1<grids.size()) {
                     grid.setRight(grids.get(index+1));
                 }
                 if(index+k<=grids.size()-1) {
@@ -60,6 +69,7 @@ public class GridTraverseVideoView extends ShapedVideoView {
         for(SquareGrid grid:grids) {
             grid.draw(canvas,paint);
         }
+        time++;
         if(isAnimated) {
             try {
                 Thread.sleep(50);
@@ -95,13 +105,17 @@ public class GridTraverseVideoView extends ShapedVideoView {
             this.down = down;
         }
         public void draw(Canvas canvas,Paint paint) {
+            paint.setStyle(Paint.Style.FILL);
             if(allowed) {
                 paint.setColor(Color.parseColor("#990097A7"));
             }
             else {
                 paint.setColor(Color.parseColor("#999E9E9E"));
             }
-            canvas.drawRoundRect(new RectF(x-w/2,y-w/2,x+w/2,y+w/2),w/15,w/15,paint);
+            canvas.drawRoundRect(new RectF(x-w/2,y-w/2,x+w/2,y+w/2),w/6,w/6,paint);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.parseColor("#99424242"));
+            canvas.drawRoundRect(new RectF(x-w/2,y-w/2,x+w/2,y+w/2),w/6,w/6,paint);
         }
         public int hashCode() {
             return (int)(2*x*x+y*y);
