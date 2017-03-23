8b462d88e0be1e9eb748369ebe9ac96d6920d69a
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 11:06:30 +0530

    Adding PinJumpVideoView

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/PinJumpVideoView.java
@@ -0,0 +1,27 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+/**
+ * Created by anweshmishra on 23/03/17.
+ */
+public class PinJumpVideoView extends ShapedVideoView {
+    public PinJumpVideoView(Context context) {
+        super(context);
+    }
+    public PinJumpVideoView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public boolean shouldDraw() {
+        return true;
+    }
+    public void drawElements(Canvas canvas,Paint paint) {
+
+    }
+    public void handleTap(float x,float y) {
+
+    }
+}
