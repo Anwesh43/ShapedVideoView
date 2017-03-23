8002dc2fc10d4ec8e1f6c40ea7d6e4f2133464b9
Anwesh43 <anweshthecool0@gmail.com>
2017-03-23 11:20:25 +0530

    Adding Jumping Pin

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/PinJumpVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/PinJumpVideoView.java
@@ -24,4 +24,39 @@ public class PinJumpVideoView extends ShapedVideoView {
     public void handleTap(float x,float y) {
 
     }
+    private class JumpingPin {
+        private float x,y,radius,dir = 0,deg = 0,initRadius;
+        public JumpingPin(float x,float y,float radius) {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.initRadius = radius;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            canvas.drawLine(0,0,0,radius,paint);
+            canvas.drawCircle(0,radius+initRadius/8,initRadius/8,paint);
+            canvas.restore();
+        }
+        public void update() {
+            radius+=dir*initRadius/4;
+            if(radius>=2*initRadius) {
+                dir = -1;
+            }
+            if(radius<=initRadius) {
+                dir = 0;
+            }
+        }
+        public boolean stopped() {
+            return dir == 0;
+        }
+        public int hashCode() {
+            return (int)(x+y+dir+deg+radius);
+        }
+        public void startMoving() {
+            dir = 1;
+        }
+    }
 }
