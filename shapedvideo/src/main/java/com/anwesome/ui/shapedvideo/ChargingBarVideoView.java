7f771994d681bdb9d0fdb1995bf1f62d61365d6c
Anwesh43 <anweshthecool0@gmail.com>
2017-03-24 11:32:28 +0530

    Adding ChargingBar class

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ChargingBarVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ChargingBarVideoView.java
@@ -24,4 +24,40 @@ public class ChargingBarVideoView extends ShapedVideoView {
     public void drawElements(Canvas canvas, Paint paint) {
 
     }
+    private class ChargingBar {
+        private float x,y,w,h;
+        private int n = 0,maxN = 6,dir = 1;
+        public ChargingBar(float x,float y,float w,float h) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            canvas.save();
+            canvas.translate(x,y);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.parseColor("#99FF6F00"));
+            drawBars(canvas,paint,-w/2,(h/2));
+            canvas.drawRect(new RectF(-w/20,-h/2-w/10,w/20,-h/2),paint);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawRect(new RectF(-w/2,-h/2,w/2,h/2),paint);
+            canvas.restore();
+        }
+        public void drawBars(Canvas canvas,Paint paint,float xStart,float yStart) {
+            for(int i=0;i<n;i++) {
+                canvas.drawRect(new RectF(xStart,yStart-h/maxN,xStart+w,yStart),paint);
+                yStart-=h/maxN;
+            }
+        }
+        public void update() {
+            n+=dir;
+            if(n == maxN-1 || n == 0) {
+                dir = 0;
+            }
+        }
+        public void startMoving() {
+            dir = n == 0?1:-1;
+        }
+    }
 }
