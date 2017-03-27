0f87ef7e48f79991e7ef9737888c68ad60cfaa23
Anwesh43 <anweshthecool0@gmail.com>
2017-03-27 12:27:06 +0530

    adding camshapevideoview to draw cam shapes

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CamShapeVideoView.java
@@ -0,0 +1,24 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+
+/**
+ * Created by anweshmishra on 27/03/17.
+ */
+public class CamShapeVideoView extends ShapedVideoView {
+    public CamShapeVideoView(Context context) {
+        super(context);
+    }
+    public CamShapeVideoView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public void drawElements(Canvas canvas, Paint paint) {
+
+    }
+    public void handleTap(float x,float y) {
+
+    }
+}
