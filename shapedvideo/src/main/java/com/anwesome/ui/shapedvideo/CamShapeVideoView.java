ee57e4ba1c8a5cb5e2237ae6d0b7395856b76959
Anwesh43 <anweshthecool0@gmail.com>
2017-03-27 12:42:56 +0530

    Drawing camshapedvideoviews

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CamShapeVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CamShapeVideoView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.shapedvideo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 
 /**
@@ -26,6 +28,7 @@ public class CamShapeVideoView extends ShapedVideoView {
 
     }
     private class CamShape {
+        private boolean stopped = false;
         private float deg = 0,x,y,size;
         public CamShape(float x,float y) {
             this.x = x;
@@ -33,10 +36,27 @@ public class CamShapeVideoView extends ShapedVideoView {
             this.size = w/10;
         }
         public void draw(Canvas canvas,Paint paint) {
-            
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            drawCamShape(canvas,paint);
+            canvas.restore();
+        }
+        public void drawCamShape(Canvas canvas,Paint paint) {
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.parseColor("#99616161"));
+            canvas.drawRoundRect(new RectF(-size/2,-size/3,size/2,size/3),size/4,size/6,paint);
+            canvas.drawRoundRect(new RectF(-size/4,-size/2,size/4,size/2),size/16,size/16,paint);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(Color.parseColor("#99263238"));
+            canvas.drawCircle(0,0,size/8,paint);
         }
         public void update() {
-
+            y+=h/12;
+            deg+=20;
+            if(y>=h) {
+                stopped = true;
+            }
         }
 
     }
