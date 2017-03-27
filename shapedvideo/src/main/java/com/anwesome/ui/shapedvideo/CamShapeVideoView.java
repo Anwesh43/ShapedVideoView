2adf9da8715ffa279b41ae103c350b50b620ce12
Anwesh43 <anweshthecool0@gmail.com>
2017-03-27 12:31:30 +0530

    setting w and h

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CamShapeVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/CamShapeVideoView.java
@@ -9,6 +9,7 @@ import android.util.AttributeSet;
  * Created by anweshmishra on 27/03/17.
  */
 public class CamShapeVideoView extends ShapedVideoView {
+    private int time = 0,w,h;
     public CamShapeVideoView(Context context) {
         super(context);
     }
@@ -16,9 +17,27 @@ public class CamShapeVideoView extends ShapedVideoView {
         super(context,attrs);
     }
     public void drawElements(Canvas canvas, Paint paint) {
-
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
     }
     public void handleTap(float x,float y) {
+
+    }
+    private class CamShape {
+        private float deg = 0,x,y,size;
+        public CamShape(float x,float y) {
+            this.x = x;
+            this.y = y;
+            this.size = w/10;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            
+        }
+        public void update() {
+
+        }
 
     }
 }
