313961702340d57ed41fc85a5b8ce259c8f4f049
Anwesh43 <anweshthecool0@gmail.com>
2017-03-28 07:20:40 +0530

    Adding HeartDrawnVideoView

---
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/HeartDrawnVideoView.java
@@ -0,0 +1,23 @@
+package com.anwesome.ui.shapedvideo;
+
+import android.content.Context;
+import android.graphics.*;
+import android.util.AttributeSet;
+
+/**
+ * Created by anweshmishra on 28/03/17.
+ */
+public class HeartDrawnVideoView extends ShapedVideoView {
+    public HeartDrawnVideoView(Context context) {
+        super(context);
+    }
+    public HeartDrawnVideoView(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+    public void drawElements(Canvas canvas,Paint paint) {
+
+    }
+    public void handleTap(float x,float y) {
+
+    }
+}
