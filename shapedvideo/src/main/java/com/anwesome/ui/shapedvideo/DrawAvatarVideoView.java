a9111d76c6a975b354f54d14d6407ec09063c188
Anwesh43 <anweshthecool0@gmail.com>
2017-03-29 13:11:45 +0530

    adding avatar class to draw and update avatar

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawAvatarVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DrawAvatarVideoView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.shapedvideo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 
 /**
@@ -19,9 +21,48 @@ public class DrawAvatarVideoView extends ShapedVideoView {
         return true;
     }
     public void drawElements(Canvas canvas, Paint paint) {
-
+        paint.setStyle(Paint.Style.FILL);
     }
     public void handleTap(float x,float y) {
 
+    }
+    private class Avatar {
+        private float x,y,size,deg = 0;
+        public Avatar(float x,float y,float size) {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+        }
+        public void drawAvatar(Canvas canvas,Paint paint) {
+            paint.setColor(Color.parseColor("#99FFFFFF"));
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            drawFace(canvas,paint);
+            drawShoulderAndNeck(canvas,paint);
+            canvas.restore();
+        }
+        public void update() {
+            deg+=20;
+        }
+        private void drawFace(Canvas canvas,Paint paint) {
+            canvas.drawCircle(0,-size/4,size/8,paint);
+        }
+        private void drawShoulderAndNeck(Canvas canvas,Paint paint) {
+            Path path = new Path();
+            path.moveTo(size/2,size/2);
+            DrawingCurveUtil.drawCircularPath(path,0,size/2,size/2,360,300);
+            float newY = (float)((size/2)*(1-Math.sqrt(3)));
+            DrawingCurveUtil.drawCircularPath(path,0,newY,size/2,60,120);
+            DrawingCurveUtil.drawCircularPath(path,0,size/2,size/2,240,180);
+            path.lineTo(size/2,size/2);
+            canvas.drawPath(path,paint);
+
+
+        }
+
+        public boolean handleTap(float x,float y) {
+            return x>=this.x-size/2 && x<=this.x+size/2 && y>=this.y-size/2 && y<=this.y+size/2;
+        }
     }
 }
