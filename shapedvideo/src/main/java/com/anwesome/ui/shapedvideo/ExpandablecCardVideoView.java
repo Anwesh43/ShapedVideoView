09efe2964eec2e901c34fe7d22641abf7b3f6160
Anwesh43 <anweshthecool0@gmail.com>
2017-04-01 11:38:34 +0530

    Adding close button for expandableview

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ExpandablecCardVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/ExpandablecCardVideoView.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
  * Created by anweshmishra on 01/04/17.
  */
 public class ExpandablecCardVideoView extends ShapedVideoView {
+    private int w,h,time = 0;
     public ExpandablecCardVideoView(Context context) {
         super(context);
     }
@@ -18,6 +19,11 @@ public class ExpandablecCardVideoView extends ShapedVideoView {
 
     }
     public void drawElements(Canvas canvas, Paint paint) {
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
     }
     public boolean shouldDraw() {
         return true;
@@ -33,6 +39,35 @@ public class ExpandablecCardVideoView extends ShapedVideoView {
         }
         public void handleTap(float x,float y) {
 
+        }
+    }
+    private class CloseButton {
+        private float x,y,r,deg = 0,dir = 0;
+        public CloseButton(float x,float y) {
+            this.x = x;
+            this.y = y;
+            this.r = w/20;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            for(int i=0;i<2;i++) {
+                canvas.save();
+                canvas.rotate(i*90);
+                canvas.drawLine(-r,0,r,0,paint);
+                canvas.restore()
+            }
+            canvas.restore();
+        }
+        public void update() {
+            deg+=9*dir;
+            if(deg >= 90 || deg<=0) {
+                dir = 0;
+            }
+        }
+        public void startMoving() {
+            dir = deg == 0?1:-1;
         }
     }
 }
