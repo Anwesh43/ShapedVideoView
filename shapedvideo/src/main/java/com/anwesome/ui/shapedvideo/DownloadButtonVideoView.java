fb55d1a8a69cac3a4e93761a54fcfa192330845d
Anwesh43 <anweshthecool0@gmail.com>
2017-04-03 06:25:53 +0530

    Adding DownloadButton

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DownloadButtonVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/DownloadButtonVideoView.java
@@ -8,6 +8,7 @@ import android.util.AttributeSet;
  * Created by anweshmishra on 03/04/17.
  */
 public class DownloadButtonVideoView extends ShapedVideoView {
+    private int time = 0,w,h;
     public DownloadButtonVideoView(Context context) {
         super(context);
     }
@@ -18,9 +19,59 @@ public class DownloadButtonVideoView extends ShapedVideoView {
         return true;
     }
     public void drawElements(Canvas canvas, Paint paint) {
-
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
     }
     public void handleTap(float x,float y) {
-        
+
+    }
+    private class DownloadButton {
+        private int mode = 0;
+        private float x,y,deg = -180,k = 0,size;
+        public DownloadButton(float x,float y) {
+            this.x = x;
+            this.y = y;
+            this.size = w/10;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            paint.setColor(Color.parseColor("#99FFFFFF"));
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.rotate(deg);
+            canvas.drawRect(new RectF(-size/2,-size/2,size/2,-size/3),paint);
+            canvas.save();
+            canvas.translate(0,k);
+            Path path = new Path();
+            path.moveTo(-size/3,-size/3);
+            path.lineTo(-size/3,size/3);
+            path.lineTo(-size/2,size/3);
+            path.lineTo(0,size/2);
+            path.lineTo(size/2,size/3);
+            path.lineTo(size/2,-size/3);
+            canvas.drawPath(path,paint);
+            canvas.restore();
+            canvas.restore();
+        }
+        public void update() {
+            switch(mode) {
+                case 0:
+                    deg+=20;
+                    if(deg >=0){
+                        mode = 1;
+                    }
+                    break;
+                case 1:
+                    k+=20;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public int hashCode() {
+            return (int)(x+y+deg);
+        }
     }
 }
