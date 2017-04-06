57db837afc4ba0477c068b4816969421daef9a39
Anwesh43 <anweshthecool0@gmail.com>
2017-04-06 14:48:28 +0530

    Adding FilterBar class

--- shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/FilterBarVideoView.java
+++ shapedvideo/src/main/java/com/anwesome/ui/shapedvideo/FilterBarVideoView.java
@@ -2,13 +2,17 @@ package com.anwesome.ui.shapedvideo;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 
 /**
  * Created by anweshmishra on 06/04/17.
  */
 public class FilterBarVideoView extends ShapedVideoView {
+    private int time = 0,w,h;
     public FilterBarVideoView(Context context) {
         super(context);
     }
@@ -19,10 +23,40 @@ public class FilterBarVideoView extends ShapedVideoView {
         return true;
     }
     public void drawElements(Canvas canvas, Paint paint) {
-
+        if(time == 0) {
+            w = canvas.getWidth();
+            h = canvas.getHeight();
+        }
+        time++;
     }
     public void handleTap(float x,float y) {
 
     }
-
+    private class FilterBar {
+        private float x,y,dir = 0,hSize = 0;
+        public FilterBar(float x,float y) {
+            this.x = x;
+            this.y = y;
+            this.hSize = h/10;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            paint.setColor(Color.parseColor("#AA4DD0E1"));
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.drawRect(new RectF(0,0,w,hSize),paint);
+            canvas.restore();
+        }
+        public void update() {
+            hSize+=(h/10)*dir;
+            if(hSize<=h/10 || hSize>=h) {
+                dir = 0;
+            }
+            if(hSize>=h) {
+                dir = 0;
+            }
+        }
+        public int hashCode() {
+            return (int)(x+y+dir);
+        }
+    }
 }
